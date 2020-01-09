fa818ad4cb556bf81d09ccec6110a936875f361a
Bryan Riel <bryanvriel@gmail.com>
2020-01-09 13:22:22 -0500

    Update periodic forcing function.

--- iceutils/sim/forces.py
+++ iceutils/sim/forces.py
@@ -8,8 +8,8 @@ class CalvingForce:
     Encapsulates a time-varying forcing function at calving front.
     """
 
-    def __init__(self, A, n=3, mode='constant', cval=1.0, period=1.0, rho_ice=917.0,
-                 fs=None, rho_water=1024.0, g=9.80665):
+    def __init__(self, A, n=3, fs=None, mode='constant', cval=1.0, period=1.0, amplitude=0.5,
+                 rho_ice=917.0, rho_water=1024.0, g=9.80665):
         """
         Store parameters.
         """
@@ -25,13 +25,7 @@ class CalvingForce:
         elif mode == 'constant':
             self.fs = lambda t: cval
         elif mode == 'periodic':
-            def pfun(t):
-                cos_amp = 0.5
-                if t < (0.5 * period):
-                    return (1 + cos_amp) - cos_amp * np.cos(2.0 * np.pi / period * t)
-                else:
-                    1.0
-            self.fs = pfun
+            self.fs = lambda t: amplitude * np.sin(2.0 * np.pi / period * t) + 1.0
         elif mode == 'step':
             self.fs = lambda t: 1.5 if t < 0.5 else 1.0
         else:
