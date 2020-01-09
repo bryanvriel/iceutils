7feb0c3e81fedcce16592dddfd877f24bb8d9a88
Bryan Riel <bryanvriel@gmail.com>
2020-01-09 13:20:51 -0500

    Adding scaling keyword argument.

--- iceutils/sim/optimize.py
+++ iceutils/sim/optimize.py
@@ -4,13 +4,15 @@ import numpy as np
 from scipy import optimize
 import sys
 
-def find_root(profile, model, method='newton', n_iter=500, tol=1.0e-5, options=None):
+def find_root(profile, model, method='newton', n_iter=500, tol=1.0e-5, scale=1.0e-2,
+              options=None):
     """
     Wrapper for various root-finding optimization algorithms. For any method other than 'newton',
     it calls scipy.optimize.root.
     """
     if method == 'newton':
-        U, F = _find_root_newton(profile, model, n_iter=n_iter, tol=tol, **options)
+        U, F = _find_root_newton(profile, model, n_iter=n_iter, tol=tol, scale=scale,
+                                 **options)
     else:
         result = optimize.root(model.compute_pde_values, profile.u,
                                jac=model.compute_jacobian, method=method,
@@ -21,8 +23,8 @@ def find_root(profile, model, method='newton', n_iter=500, tol=1.0e-5, options=N
     # Done
     return U, F
 
-def _find_root_newton(profile, model, n_iter=500, tol=1.0e-5, reltol=1.0e-10, delta=0.2,
-                      rcond=1.0e-10):
+def _find_root_newton(profile, model, n_iter=500, tol=1.0e-5, scale=1.0e-2,
+                      reltol=1.0e-10, delta=0.2, rcond=1.0e-10):
     """
     Implements Newton's method for finding the roots of a multivariate function. Function
     is provided by model.compute_pde_values().
@@ -35,7 +37,7 @@ def _find_root_newton(profile, model, n_iter=500, tol=1.0e-5, reltol=1.0e-10, de
     for i in range(n_iter):
 
         # Compute value of PDE at current point
-        F = model.compute_pde_values(U)
+        F = model.compute_pde_values(U, scale=scale)
         Fmag = np.linalg.norm(F)
 
         # Diagnostics
@@ -50,7 +52,7 @@ def _find_root_newton(profile, model, n_iter=500, tol=1.0e-5, reltol=1.0e-10, de
             break
 
         # Compute Jacobian at current point
-        J = model.compute_jacobian(U)
+        J = model.compute_jacobian(U, scale=scale)
 
         # Compute update vector
         dU = np.linalg.lstsq(J, -F, rcond=rcond)[0]
