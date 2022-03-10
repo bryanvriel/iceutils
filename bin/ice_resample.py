2260c5f39be5a705fe9cf4c2a91244c56cf879ee
Bryan Riel <bryanvriel@gmail.com>
2022-03-10 21:33:10 +0800

    Can force EPSG codes for warping.

--- bin/ice_resample.py
+++ bin/ice_resample.py
@@ -16,8 +16,12 @@ def parse():
         help='Output raster/stack file.')
     parser.add_argument('-epsg', action='store', type=int, default=None,
                         help='Force outputs EPSG code.')
+    parser.add_argument('-srs_epsg', action='store', type=int, default=None,
+                        help='Force source EPSG code.')
     parser.add_argument('-driver', action='store', type=str, default='ENVI',
                         help='GDAL driver. Default: ENVI.')
+    parser.add_argument('-b', action='store', type=int, default=1, dest='band',
+                        help='Raster band to resample. Default: 1.')
     parser.add_argument('-keys', action='store', type=str, nargs='+', default=['data', 'weights'],
         help='List of stack keys to resample. Default: data, weights.')
     return parser.parse_args()
@@ -45,19 +49,32 @@ def main(args):
     else:
         
         # Input raster
-        inobj = ice.Raster(rasterfile=args.raster)
+        inobj = ice.Raster(rasterfile=args.raster, band=args.band)
+        if args.srs_epsg is not None:
+            in_epsg = args.srs_epsg
+            inobj.hdr._epsg = in_epsg
+        else:
+            in_epsg = inobj.hdr.epsg
+        print(inobj.hdr.projWin)
 
         # Reference raster
-        ref = ice.Raster(rasterfile=args.reference)
-
-        # Resample
-        inobj.resample(ref.hdr)
-
-        # Write to disk
+        ref_hdr = ice.RasterInfo(args.reference)
         if args.epsg is not None:
             out_epsg = args.epsg
         else:
-            out_epsg = ref.hdr.epsg
+            out_epsg = ref_hdr.epsg
+        print(ref_hdr.projWin)
+
+        # Direct resample if EPSG codes are the same
+        if in_epsg == out_epsg:
+            print('Resampling')
+            inobj.resample(ref_hdr)
+        else:
+            print('Warping from %d to %d' % (in_epsg, out_epsg))
+            inobj = ice.warp(inobj, target_epsg=out_epsg, target_hdr=ref_hdr,
+                             order=3, mode='constant', cval=np.nan)
+
+        # Write to disk
         inobj.write_gdal(args.output, epsg=out_epsg, driver=args.driver)
 
         
